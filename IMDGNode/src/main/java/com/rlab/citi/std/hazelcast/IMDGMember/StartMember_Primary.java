ba95877fc9c4fcf68c41d9f4058e2ae1a61aa499
rimogitz <rimogit1@gmail.com>
2020-07-06 18:12:17 +0100

    IMDG member for Lab

---
+++ IMDGNode/src/main/java/com/rlab/citi/std/hazelcast/IMDGMember/StartMember_Primary.java
@@ -0,0 +1,25 @@
+package com.rlab.citi.std.hazelcast.IMDGMember;
+
+import java.io.FileNotFoundException;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.XmlConfigBuilder;
+import com.hazelcast.core.Hazelcast;
+
+/**
+ * Hello world!
+ *
+ */
+public class StartMember_Primary 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Starting Hazelcast" );
+        try {
+			Hazelcast.newHazelcastInstance(new XmlConfigBuilder("src/main/resources/hazelcast_PRIMARY.xml").build());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+}
